1574f1ac9f1374ee37214e4a3df2348227f3121f
ilhami <ilhami@unknown468f4a8d6f31.home>
2025-05-01 17:36:00 +0300

    Add render build script and update configs

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const app = express();
 const server = http.createServer(app);
 
 const settings = {
-    httpAdminRoot: "/admin",
-    httpNodeRoot: "/",
-    userDir: "./data",
-    flowFile: "flows.json",
+  httpAdminRoot: "/",
+  httpNodeRoot: "/api",
+  userDir: "./data",
+  functionGlobalContext: {},
+  ui: { path: "/ui" },
 };
 
 RED.init(server, settings);
@@ -17,6 +18,7 @@ RED.init(server, settings);
 app.use(settings.httpAdminRoot, RED.httpAdmin);
 app.use(settings.httpNodeRoot, RED.httpNode);
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 1880);
 
 RED.start();
+
